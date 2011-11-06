077fc0c674e3fe19839b557630280e2919200adc
Wasser <wasserx@hotmail.com>
2011-11-06 17:10:59 +0100

    Buttons logic added

--- FormGWT/src/formGWT/client/FormGWT.java
+++ FormGWT/src/formGWT/client/FormGWT.java
@@ -29,13 +29,14 @@ import com.google.gwt.user.datepicker.client.DatePicker;
  */
 public class FormGWT implements EntryPoint {
 	
-	private Button sendButton = new Button("Send");
 	private Button prevButton = new Button("Previous");
 	private Button nextButton = new Button("Next");
-	/*First Page*/
+	private int PAGE = 1;
+	//First Page
 	private Grid firstPageContent = new Grid(7, 2);
 	private TextBox nameField = new TextBox();
 	private TextBox lastNameField = new TextBox();
+	private TextBox birthField = new TextBox();
 	
 	private HorizontalPanel navPanel = new HorizontalPanel();
 	
@@ -54,27 +55,74 @@ public class FormGWT implements EntryPoint {
 			.create(GreetingService.class);
 	
 	/**
-	 * Updates the navigation panel after a change on the website (enables/disables buttons).
-	 * TODO: Only allows sending after all fields are full and we are at the last page.
+	 * Updates the wepage content and buttons according to the current page
+	 * 
 	 */
-	private void updateNavPanel() {	
+	private void updateView() {	
+		switch(PAGE){
+		case 1:
+			prevButton.setEnabled(false);
+			nextButton.setEnabled(true);
+			nextButton.addClickHandler(new ClickHandler() {
+				
+				@Override
+				public void onClick(ClickEvent event) {
+					firstPage(false);
+					//secondPage(true);
+					
+				}
+			});
+			break;
+		case 2:
+			prevButton.setEnabled(true);
+			prevButton.addClickHandler(new ClickHandler() {
+				
+				@Override
+				public void onClick(ClickEvent event) {
+					firstPage(true);
+					//secondPage(false);
+				}
+			});
+			nextButton.setEnabled(true);
+			nextButton.addClickHandler(new ClickHandler() {
+				
+				@Override
+				public void onClick(ClickEvent event) {
+					//secondPage(false);
+					//thirdPage(true);
+				}
+			});
+			break;
+		case 3:
+			//TODO: Case 3
+			break;
+		}
+		
 	}
 
 	
-	//TODO: Deal with the change of pages.
-	private void drawFirstPage() {
+	/**
+	 * Sets up the first page of the form.
+	 * @param state Shows or hides the content
+	 */
+	private void firstPage(boolean state) {
 		Label cityLabel = new Label("Hometown:");
 		Label genderLabel = new Label("Gender:");
 		Label mailLabel = new Label("E-mail Address:");
 	
+		//Form contents
 		firstPageContent.setWidget(0, 0, new Label("Name:"));
 		firstPageContent.setWidget(0, 1, nameField);
 		firstPageContent.setWidget(1, 0, new Label("Last Name:"));
 		firstPageContent.setWidget(1, 1, lastNameField);
 		firstPageContent.setWidget(2, 0, new Label("Date of Birth:"));
-		//content.setWidget(2,1, birthPicker);
+		firstPageContent.setWidget(2, 1, birthField);
 		firstPageContent.setWidget(3, 0, new Label("Birth Country:"));
 		
+		RootPanel.get().add(firstPageContent);
+		
+		PAGE = 1;
+		updateView();
 	}
 	
 	/**
@@ -83,12 +131,9 @@ public class FormGWT implements EntryPoint {
 	public void onModuleLoad() {
 	
 		navPanel.add(prevButton);
-		navPanel.add(sendButton);
 		navPanel.add(nextButton);
 		
-		drawFirstPage();
-
-		RootPanel.get().add(firstPageContent);
-		
+		firstPage(true);
+		RootPanel.get().add(navPanel);
 	}
 }
